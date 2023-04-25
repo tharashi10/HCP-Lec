1de893e4383434af9ad0a6bf108de79131bdebc6
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2023-04-25 16:31:16 +0900

    init

--- network.tf
+++ network.tf
@@ -6,6 +6,9 @@ resource "oci_core_vcn" "dev_vcn" {
 }
 
 # VCN
+variable "compartment_ocid" {
+  default = "ocid1.compartment.oc1..aaaaaaaatyksv4n5nww3nm3jjc6mcfw6fqlsuafj7h63nqn22zkqlochrava"
+}
 variable "vcn_cidr_block" {
   default = "10.51.0.0/16"
 }
