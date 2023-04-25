07d618cb7af296871ea7002bd87873f5c52f01d8
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2023-04-25 15:28:54 +0900

    init

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "oci" {
+  tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaamu4bvfwi2pkyutoags7ws3t6jnfrbfwgcwiwzqziuwuikunj772q"
+  user_ocid        = "ocid1.user.oc1..aaaaaaaaxv3cp6tqepzq66japmdmnobgh5jlrixahkgagwpiz322jvvu5rqq"
+  private_key_path = ""
+  fingerprint      = ""
+  region           = "ap-tokyo-1"
+}
