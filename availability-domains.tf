235967b20b850c89c605f71b7ef71abfc5b6b5b7
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2023-05-01 17:42:08 +0900

    datasource

--- availability-domains.tf
+++ availability-domains.tf
@@ -2,10 +2,16 @@ data "oci_identity_availability_domains" "ads" {
   compartment_id = var.compartment_ocid
 }
 
-data "oci_core_services" "dev_services" {}
+data "oci_core_services" "dev_services" {
+  filter {
+    name = "name"
+    values = ["All .* Services In Oracle Services Network"]
+    regex = true
+  }
+}
 
 output "sample_services" {
-  value = [data.oci_core_services.dev_services.services]
+  value = data.oci_core_services.dev_services.services
 }
 
 output "all-ads" {
