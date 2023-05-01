a85d28958433d48f6121f4220423e7751734151e
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2023-05-01 15:27:09 +0900

    seclist

--- network.tf
+++ network.tf
@@ -8,7 +8,22 @@ resource "oci_core_vcn" "dev_vcn" {
   dns_label      = var.vcn_dns_label
 }
 
-output "oci_dev_vcn_id" {
-  description = "Cheking ID of the dev_vcn"
-  value = oci_core_vcn.dev_vcn.id
+resource "oci_core_security_list" "dev_security_list_prv" {
+  compartment_id = var.compartment_ocid
+  egress_security_rules {
+    destination = var.sl_egress_destination_prv
+    protocol    = var.sl_egress_protocol_prv
+    stateless   = false
+  }
+  ingress_security_rules {
+    source     = var.sl_ingress_source_prv     
+    protocol   = var.sl_ingress_protocol_prv
+    stateless  = false
+    tcp_options {
+      max = var.sl_ingress_tcp_dest_port_max_prv
+      min = var.sl_ingress_tcp_dest_port_min_prv
+    }
+  }
+  vcn_id       = oci_core_vcn.dev_vcn.id
+  display_name = var.sl_display_name_prv
 }
