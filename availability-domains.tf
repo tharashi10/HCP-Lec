bb3f66b1f23a8cfa71e530e35518bde7033d7380
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2023-05-01 17:19:57 +0900

    sgw

--- availability-domains.tf
+++ availability-domains.tf
@@ -2,6 +2,12 @@ data "oci_identity_availability_domains" "ads" {
   compartment_id = var.compartment_ocid
 }
 
+data "oci_core_services" "dev_services" {}
+
+output "sample_services" {
+  value = [data.oci_core_services.dev_services.services]
+}
+
 output "all-ads" {
   value = data.oci_identity_availability_domains.ads.availability_domains
 }
