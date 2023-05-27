ea2f348990d1f817d76325053318a0cbd4ac58d4
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2023-05-27 23:48:24 +0900

    for-each

--- getting-started/oci-iam.tf
+++ getting-started/oci-iam.tf
@@ -1,23 +1,22 @@
-resource "oci_identity_user" "sample_user" {
+// resource "oci_identity_user" "sample_user" {
+//   compartment_id = oci_identity_compartment.default.id
+//   description    = "oci sample user"
+//   name           = var.user_names[count.index]
+//   count          = length(var.user_names)
+//   defined_tags   = { "Owner" = "TeamXYZ", "ManagedBy" = "Terraform" }
+// }
+
+resource "oci_identity_user" "sample_users_for_each" {
+  for_each       = toset(var.user_names) // set or map
+  name           = each.value
   compartment_id = oci_identity_compartment.default.id
   description    = "oci sample user"
-  name           = var.user_names[count.index]
-  count          = length(var.user_names)
   defined_tags   = { "Owner" = "TeamXYZ", "ManagedBy" = "Terraform" }
 }
 
-//resource "oci_identity_user" "sample_users_for_each" {
-//  for_each = toset(var.user_names) // set or map
-//  name     = each.value
-//}
-
 output "all_users" {
-  value = oci_identity_user.sample_user
+  value = oci_identity_user.sample_users_for_each
 }
-
-//output "all_users_ids" {
-//  value = values(oci_identity_user.sample_users_for_each)[*].id
-//}
 
 output "all_users_ids_list_compression" {
   value = [for name in var.user_names : upper(name)]
