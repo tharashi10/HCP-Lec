3845e5cca33c634fe32ff161e4cfffaf52db974b
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2023-10-15 23:09:16 +0900

    workflows

--- provider.tf
+++ provider.tf
@@ -1,20 +1,28 @@
-#
-# Provider ファイル
-#
+/*
+ * Terraformのバージョンを指定
+ * Terraform v1.4.6 (2023/05.時点) CloudShellに含まれているTerraform Version
+ */
 terraform {
   required_version = ">= 1.4.0"
 }
 
-# Instance PrincipalでAPI実行する
+/*
+ * プロバイダを指定
+ * Terraform initを実行する際に、どのプロバイダのAPIとするのかを指定している
+ * 本Terraformファイルでは「インスタンス・プリンシパル認可」の方式でAPIをコールしている
+ */
 provider "oci" {
   auth = "InstancePrincipal"
 }
 
-# Not necessary for using instance principal in Cloud Shell.
-# provider "oci" {
-#   tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaamu4bvfwi2pkyutoags7ws3t6jnfrbfwgcwiwzqziuwuikunj772q"
-#   user_ocid        = "ocid1.user.oc1..aaaaaaaaxv3cp6tqepzq66japmdmnobgh5jlrixahkgagwpiz322jvvu5rqq"
-#   private_key_path = ""
-#   fingerprint      = ""
-#   region           = "ap-tokyo-1"
-# }
+/* 
+ * デフォルトの場合、以下のような「API Key認証」でAPIをコールする
+ *
+provider "oci" {
+  tenancy_ocid     = "ocid1.tenancy.oc1..aaaaanfrbf___ziuwuikunj772q"
+  user_ocid        = "ocid1.user.oc1..aaaaaaaax____6jhkga322jvvu5rqq"
+  private_key_path = ""
+  fingerprint      = ""
+  region           = "ap-tokyo-1"
+}
+*/
